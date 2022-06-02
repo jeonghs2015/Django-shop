80c53fd8a29344a469a0b83bf50b7ae5513e7a35
jeonghs2015 <hsjung201558@gmail.com>
2022-06-02 12:12:52 +0900

    DB 연동 작업

--- config/settings.py
+++ config/settings.py
@@ -74,10 +74,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST' : 'db.gnupy.shop',
+        'NAME': 'dbgnupy',
+        'USER' : 'gnupy',
+        'PASSWORD' : 'dptmdkdldkf1!',
+        'PORT' : '5432',
     }
 }
 
