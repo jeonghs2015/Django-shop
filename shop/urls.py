0367fab76f46a9e3c0c4925a35d858b4142cbb71
jeonghs2015 <hsjung201558@gmail.com>
2022-06-02 14:17:22 +0900

    shop/urls path 설정

---
+++ shop/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+app_name = 'shop'
+urlpatterns = [
+    path('', product_in_category, name='product_all'),
+    path('<slug:category_slug>/', product_in_category, name='product_all'),
+    path('<ing:id>/<product_slug>/'),
+]
