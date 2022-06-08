e0969e0d1f11011b606c7ba023fdd9e1613c49a2
jeonghs2015 <hsjung201558@gmail.com>
2022-06-08 15:27:27 +0900

    cart 관련 view 페이지 구현

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+# POST로만 접근할 수 있도록 해주는 구문
 
 # Create your views here.
+
+from shop.models import Product
+from .forms import AddProductForm
+from .cart import Cart
+
+def add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+
+    # 클라이언트 -> 서버로 데이터를 전달
+    # 유효성 검사, injection 전처리
+    form = AddProductForm(request.POST)
+
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], is_update=cd['is_update'])
+
+        return redirect('cart:detail')
