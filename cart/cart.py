e3353750d2c66f3c16d421a233084be3e0378208
jeonghs2015 <hsjung201558@gmail.com>
2022-06-09 15:08:30 +0900

    Cart class에 coupon 관련 메소드 추가

--- cart/cart.py
+++ cart/cart.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from requests import request
 
 from shop.models import Product
+from coupon.models import Coupon
 
 class Cart(object):
     def __init__(self, request):     #초기화 작업
@@ -11,6 +12,7 @@ class Cart(object):
         if not cart:
             cart = self.session[settings.CART_ID] = {}
         self.cart = cart
+        self.coupon_id = self.session.get('coupon_id')
         
 
     def __len__(self):      #list, dictionary에 사용되는 메서드
@@ -55,8 +57,24 @@ class Cart(object):
 
     def clear(self):
         self.session[settings.CART_ID] = {}
+        self.session['coupon_id'] = None
         self.session.modified = True
 
     def get_product_total(self):
         return sum(item['price']*item['quantity'] for item in self.cart.values())
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            return Coupon.objects.get(id=self.coupon_id)
+
+    def get_discount_total(self):
+        if self.coupon:
+            if self.get_product_total() >= self.coupon.amount:
+                return self.coupon.amount
+        return Decimal(0)
+
+
+    def get_total_price(self):
+        return self.get_product_total() - self.get_discount_total()
 
