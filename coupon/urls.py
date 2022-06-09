57b983469569f7506797f6eb0bf2b58e4b83f2f7
jeonghs2015 <hsjung201558@gmail.com>
2022-06-09 15:38:12 +0900

    add coupon 기능 작업

--- coupon/urls.py
+++ coupon/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-
 from .views import add_coupon
 
-app_name = 'coupon'
+app_name='coupon'
 
 urlpatterns = [
-    path('add/'),
-
+    path('add/', add_coupon, name='add'),
 ]
