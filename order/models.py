541b13cb7fef99b30693c7c65f33359e5e7af002
jeonghs2015 <hsjung201558@gmail.com>
2022-06-10 17:36:41 +0900

    OrderTransaction Manager 구현 작업

--- order/models.py
+++ order/models.py
@@ -55,3 +55,42 @@ class OrderItem(models.Model):
 
     def get_tiem_price(self):
         return self.price * self.quantity
+
+import hashlib
+from .iamport import payments_prepare, find_transaction
+class OrderTransactionManager(models.Manager):
+    def create_new(self, order, amount, success=None, transaction_status=None):
+        if not order:
+            raise ValueError("주문 정보 오류")
+        
+        order_hash = hashlib.sha1(str(order.id).encode('utf-8')).hexdigest()
+        email_hash = str(order.email).split("@")[0]
+        final_hash = hashlib.sha1((order_hash+email_hash).encode('utf-8')).hexdigest()[:10]
+        merchant_order_id = str(final_hash)
+        payments_prepare(merchant_order_id, amount)
+        transaction = self.model(
+            order = order,
+            merchant_order_id = merchant_order_id,
+            amount = amount
+        )
+
+        if success is not None:
+            transaction.success = success
+            transaction.transaction_status = transaction_status
+
+        try:
+            transaction.save()
+        except Exception as e:
+            print("save error", e)
+
+        return transaction.merchant_order_id
+
+    def get_transaction(self, merchant_order_id):
+        result = find_transaction(merchant_order_id)
+        if result['status'] == 'paid':
+            return result
+        else:
+            return None
+
+class OrderTransaction(models.Model):
+    pass
