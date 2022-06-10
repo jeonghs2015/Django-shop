19256435a4d67a95e839491f6f4567e0284a1062
jeonghs2015 <hsjung201558@gmail.com>
2022-06-10 10:49:22 +0900

    iamport get_token -> iamport 로그인 관련 함수 구현

---
+++ order/iamport.py
@@ -0,0 +1,30 @@
+import requests
+from django.conf import settings
+
+# settings.py에 있는 API key, Secret key 를 가지고 IAMport에 가서 로그인 해주는 기능
+def get_token():
+    access_data = {
+        'imp_key':settings.IAMPORT_KEY,
+        'imp_secret':settings.IAMPORT_SECRET
+    }
+    url = "https://api.iamport.kr/users/getToken"
+    # 크롤링 할때 사용하는 requests
+    req = requests.post(url, data=access_data)
+    # 크롤링할때는 보통 html 형태로 받아오지만 우리는 지금 api를 사용하기때문에 json 형태로 받아오게됨.
+    access_res = req.json()
+
+    if access_res['code'] is 0:
+        return access_res['response']['access_token']
+    else:
+        return None
+
+# 어떤 order_id로 얼마의 금액으로 결제를 요청할 건지 IAMport에 등록해주는 함수.
+def payments_prepare(order_id, amount, *args, **kwargs):
+    pass
+
+
+# 결제가 이뤄지면 iamport 에 기록이 남고 django app으로 결제 완료된 정보를 전송해준다.
+# 주의할 점은 실제 결제 완료된 금액과 django app에서 결제된 금액이 맞는지 확인해야함.
+# 그 과정을 진행하는 함수
+def find_transaction(order_id, *args, **kwargs)
+    pass
