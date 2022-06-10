a222cbe2704eb3807de7d4842d801ea09a8788df
jeonghs2015 <hsjung201558@gmail.com>
2022-06-10 11:33:25 +0900

    order(주문)관련 modeling 작업

--- order/models.py
+++ order/models.py
@@ -1,3 +1,27 @@
+from ctypes import addressof
+import email
+from operator import truediv
+from ssl import create_default_context
+from types import CoroutineType
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from coupon.models import Coupon
 
 # Create your models here.
+class Order(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    postal_code = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+
+    coupon = models.ForeignKey(Coupon, on_delete=models.PROTECT, related_name='order_coupon', null=True, blank=True)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100000)])
+
+    class Meta:
+        ordering = ['-created']
+        
