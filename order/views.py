7d64b1addc9a94a665f33d252bfbc9199a3697f8
jeonghs2015 <hsjung201558@gmail.com>
2022-06-13 15:38:14 +0900

    주문건 view 메소드 구현

--- order/views.py
+++ order/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import *
+from cart.cart import Cart
+from .forms import *
 # Create your views here.
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        # 입력받는 정보를 후처리
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.coupon.amount
+                order.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            cart.clear()
+            return render(request, 'order/created.html', {'order':order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'order/create.html', {'cart':cart,'form':form})
