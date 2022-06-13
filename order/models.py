a4f06c5403745cab23420eacbe24c26d79445a5a
jeonghs2015 <hsjung201558@gmail.com>
2022-06-13 11:00:07 +0900

    oRDERtRANSACTION POST_SAVE 시그널 연결하기

--- order/models.py
+++ order/models.py
@@ -108,3 +108,18 @@ class OrderTransaction(models.Model):
 
     class Meta:
         ordering = ['-created']
+
+def order_payment_validation(sender, instance, created, *args, **kwargs):
+    if instance.transaction_id:
+        import_transaction = OrderTransaction.objects.get_transaction(merchant_order_id=instance.merchant_order_id)
+        merchant_order_id = import_transaction['merchant_order_id']
+        imp_id = import_transaction['imp_id']
+        amount = import_transaction['amount']
+
+        local_transaction = OrderTransaction.objects.filter(merchant_order_id=merchant_order_id, transaction_id=imp_id, amount=amount).exists()
+
+        if not import_transaction or not local_transaction:
+            raise ValueError("비정상 거래입니다.")
+
+from django.db.models.signals import post_save
+post_save.connect(order_payment_validation, sender=OrderTransaction)
