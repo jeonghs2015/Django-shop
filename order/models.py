d629540c4885f4fb516b37c433ccc09a1d6b79d8
jeonghs2015 <hsjung201558@gmail.com>
2022-06-13 10:08:07 +0900

    ordertransaction 모델링 작업, migrations

--- order/models.py
+++ order/models.py
@@ -93,4 +93,18 @@ class OrderTransactionManager(models.Manager):
             return None
 
 class OrderTransaction(models.Model):
-    pass
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    merchant_order_id = models.CharField(max_length=120, null=True, blank=True)
+    transaction_id = models.CharField(max_length=120, null=True, blank=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    transaction_status = models.CharField(max_length=220, null=True, blank=True)
+    type = models.CharField(max_length=120, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    objects = OrderTransactionManager()
+
+    def __str__(self):
+        return str(self.order.id)
+
+    class Meta:
+        ordering = ['-created']
