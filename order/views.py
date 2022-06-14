56890de884e6c85c88de56507ccc4013d7f19a72
jeonghs2015 <hsjung201558@gmail.com>
2022-06-14 09:50:57 +0900

    order views 페이지 할인 금액까지 구현 완료

--- order/views.py
+++ order/views.py
@@ -10,11 +10,11 @@ def order_create(request):
         # 입력받는 정보를 후처리
         form = OrderCreateForm(request.POST)
         if form.is_valid():
-            order = form.save()
+            order = form.save(commit=False)
             if cart.coupon:
                 order.coupon = cart.coupon
-                order.discount = cart.coupon.amount
-                order.save()
+                order.discount = cart.get_discount_total
+            order.save()
             for item in cart:
                 OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
             cart.clear()
@@ -32,4 +32,28 @@ def order_complete(request):
     return render(request, 'order/created.html', {'order':order})
 
 
-    
+from django.views.generic.base import View
+from django.http import JsonResponse
+
+class OrderCreateAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({"authenticated":False}, status=403)
+
+        cart = Cart(request)
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            if cart.coupon:
+                order.coupon = cart.coupon
+                order.discount = cart.get_discount_total
+            order.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            cart.clear()
+            data = {
+                "order_id":order.id
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
