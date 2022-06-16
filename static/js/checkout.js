eb1ea138b7c38928a42c07fa06f6af66c1594635
jeonghs2015 <hsjung201558@gmail.com>
2022-06-16 15:59:19 +0900

    오류 수정 작업 2

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -1,29 +1,35 @@
-$(function(){
-    var IMP=window.IMP;
+$(function () {
+    var IMP = window.IMP;
     IMP.init('imp11166264');
-    $('.order-form').on('submit', function(e) {
-        var amount = parseFloat($('.order-form input[name="amount"]').val().replace(',',''));
-        var type=$('.order-form input[name="type"]:checked').val();
+    $('.order-form').on('submit', function (e) {
+        var amount = parseFloat($('.order-form input[name="amount"]').val().replace(',', ''));
+        var type = $('.order-form input[name="type"]:checked').val();
+        // 폼 데이터를 기준으로 주문 생성
         var order_id = AjaxCreateOrder(e);
-        if(order_id==false){
+        if (order_id == false) {
             alert('주문 생성 실패\n다시 시도해주세요.');
             return false;
         }
 
+        // 결제 정보 생성
         var merchant_id = AjaxStoreTransaction(e, order_id, amount, type);
 
-        if(merchant_id!=='') {
+        // 결제 정보가 만들어졌으면 iamport로 실제 결제 시도
+        if (merchant_id !== '') {
             IMP.request_pay({
-                merchant_uid:merchant_id,
-                name:'E-Shop product',
+                merchant_uid: merchant_id,
+                name: 'E-Shop product',
                 buyer_name:$('input[name="first_name"]').val()+" "+$('input[name="last_name"]').val(),
-                buyer_email:$('input[name="email"]'),
-                amount:amount
-            }, function(rsp) {
-                if(rsp.success){
+                buyer_email:$('input[name="email"]').val(),
+                amount: amount
+            }, function (rsp) {
+                if (rsp.success) {
                     var msg = '결제가 완료되었습니다.';
-                    msg += '고유 ID : '+rsp.imp_uid;
-                    // 결제 완료 후 보여줄 메시지
+                    msg += '고유ID : ' + rsp.imp_uid;
+                    msg += '상점 거래ID : ' + rsp.merchant_uid;
+                    msg += '결제 금액 : ' + rsp.paid_amount;
+                    msg += '카드 승인번호 : ' + rsp.apply_num;
+                    // 결제가 완료되었으면 비교해서 디비에 반영
                     ImpTransaction(e, order_id, rsp.merchant_uid, rsp.imp_uid, rsp.paid_amount);
                 } else {
                     var msg = '결제에 실패하였습니다.';
@@ -36,94 +42,94 @@ $(function(){
     });
 });
 
-// e : javascript event
-// Order 내용을 생성해주는 함수
+// 폼 데이터를 기준으로 주문 생성
 function AjaxCreateOrder(e) {
-    // form이 submit 되는 현상을 막아줌
     e.preventDefault();
     var order_id = '';
     var request = $.ajax({
-        method:'POST',
-        url:order_create_url,
-        async:false,         // 결제 과정이 꼬이지 않게 동기 과정으로 진행
-        data:$('.order-form').serialize()
+        method: "POST",
+        url: order_create_url,
+        async: false,
+        data: $('.order-form').serialize()
     });
-    request.done(function(data){
-        if(data.order_id) {
+    request.done(function (data) {
+        if (data.order_id) {
             order_id = data.order_id;
         }
     });
-    request.fail(function(jqXHR, textStatus) {
-        if(jqXHR.status == 404) {
+    request.fail(function (jqXHR, textStatus) {
+        if (jqXHR.status == 404) {
             alert("페이지가 존재하지 않습니다.");
-        }else if(jqXHR.status == 403) {
-            alert("로그인해주세요");
-        }else {
-            alert("문제가 발생했습니다.\n다시 시도해주세요.");
+        } else if (jqXHR.status == 403) {
+            alert("로그인 해주세요.");
+        } else {
+            alert("문제가 발생했습니다. 다시 시도해주세요.");
         }
     });
     return order_id;
 }
 
+// 결제 정보 생성
 function AjaxStoreTransaction(e, order_id, amount, type) {
     e.preventDefault();
     var merchant_id = '';
     var request = $.ajax({
-        method:'POST',
-        url:order_checkout_url,
-        async:false,
-        data:{
-            order_id:order_id,
-            amount:amount,
-            type:type,
-            csrfmiddlewaretoken:csrf_token,
+        method: "POST",
+        url: order_checkout_url,
+        async: false,
+        data: {
+            order_id : order_id,
+            amount: amount,
+            type: type,
+            csrfmiddlewaretoken: csrf_token,
         }
     });
-    request.done(function(data) {
-        if(data.works) {
+    request.done(function (data) {
+        if (data.works) {
             merchant_id = data.merchant_id;
         }
     });
-    request.fail(function(jqXHR, textStatus) {
-        if(jqXHR.status == 404) {
+    request.fail(function (jqXHR, textStatus) {
+        if (jqXHR.status == 404) {
             alert("페이지가 존재하지 않습니다.");
-        }else if(jqXHR.status == 403) {
-            alert("로그인해주세요");
-        }else {
-            alert("문제가 발생했습니다.\n다시 시도해주세요.");
+        } else if (jqXHR.status == 403) {
+            alert("로그인 해주세요.");
+        } else {
+            alert("문제가 발생했습니다. 다시 시도해주세요.");
         }
     });
+    return merchant_id;
 }
 
-// validation 수행작업
-// iamport에 요청을 보내서 개발중인 서버에 있는 금액정보와 같은 금액으로 결제가 되었는지 확인하는 함수
-function ImpTransaction(e, order_id, merchant_id, imp_id, amount) {
+// iamport에 결제 정보가 있는지 확인 후 결제 완료 페이지로 이동
+function ImpTransaction(e, order_id,merchant_id, imp_id, amount) {
     e.preventDefault();
     var request = $.ajax({
-        method:'POST',
-        url:order_validation_url,
-        async:false,
-        data:{
+        method: "POST",
+        url: order_validation_url,
+        async: false,
+        data: {
             order_id:order_id,
-            merchant_id:merchant_id,
-            imp_id:imp_id,
-            amount:amount,
-            csrfmiddlewaretoken:csrf_token
+            merchant_id: merchant_id,
+            imp_id: imp_id,
+            amount: amount,
+            csrfmiddlewaretoken: csrf_token
         }
     });
-    request.done(function(data){
-        if(data.works) {
+    
+    request.done(function (data) {
+        if (data.works) {
             $(location).attr('href', location.origin+order_complete_url+'?order_id='+order_id)
         }
     });
-    request.fail(function(jqXHR, textStatus) {
-        if(jqXHR.status == 404) {
+    request.fail(function (jqXHR, textStatus) {
+        if (jqXHR.status == 404) {
             alert("페이지가 존재하지 않습니다.");
-        }else if(jqXHR.status == 403) {
-            alert("로그인해주세요");
-        }else {
-            alert("문제가 발생했습니다.\n다시 시도해주세요.");
+        } else if (jqXHR.status == 403) {
+            alert("로그인 해주세요.");
+        } else {
+            console.log(jqXHR);
+            alert("문제가 발생했습니다. 다시 시도해주세요.");
         }
     });
 }
-
